4fb5a1dfa7e7c098579a0a3166e22e2ce618b349
cc <cc@ccMac-Pro.local>
2023-10-17 08:34:14 +0800

    测试

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const assert = require('node:assert');
 
 (async () => {
   // Setup
-  const browser = await chromium.launch({headless: false});
+  const browser = await chromium.launch({headless: true});
   const context = await browser.newContext();
   const page = await context.newPage();
 
